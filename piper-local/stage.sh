a576944583c47e22362c573ff1ddc2e39d69b4fd
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2015-05-15 11:10:08 +0200

    Updated stage script

--- piper-local/stage.sh
+++ piper-local/stage.sh
@@ -1,1 +1,10 @@
-../stage.sh
+function stats() {
+    STAGES=$(cat trace_${1}.txt | cut -f 4 | cut -f 1 -d ' ' | uniq | sort)
+    for x in $STAGES; do 
+      row=$(cat trace_${1}.txt | grep $x | sed 's/-/0/g' | datamash median 19 mean 19 median 20 mean 20 count 1)
+      printf "$x\t$row\n"
+    done
+}
+
+echo $1 | awk '{print toupper($0)}'
+stats $1
--- rnatoy-local/stage.sh
+++ rnatoy-local/stage.sh
@@ -1,9 +1,10 @@
 function stats() {
     STAGES=$(cat trace_${1}.txt | cut -f 4 | cut -f 1 -d ' ' | uniq | sort)
     for x in $STAGES; do 
-      row=$(cat trace_${1}.txt | grep $x | sed 's/-/0/g' | datamash median 17 mean 17 median 18 mean 18)
+      row=$(cat trace_${1}.txt | grep $x | sed 's/-/0/g' | datamash median 17 mean 17 median 18 mean 18 count 1)
       printf "$x\t$row\n"
     done
 }
 
+echo $1 | awk '{print toupper($0)}'
 stats $1
